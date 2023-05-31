89fb73769e7a087c57b2b34f30ad42d1aad6380b
Alejandro RM <alejandror45@gmail.com>
2023-05-30 23:39:50 -0600

    Done

--- app-ta/App.js
+++ app-ta/App.js
@@ -1,79 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+	const [hasPermission, setHasPermission] = useState(null);
+	const [scanned, setScanned] = useState(false);
 
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    };
+	useEffect(() => {
+		const getBarCodeScannerPermissions = async () => {
+			const { status } = await BarCodeScanner.requestPermissionsAsync();
+			setHasPermission(status === "granted");
+		};
 
-    getBarCodeScannerPermissions();
-  }, []);
+		getBarCodeScannerPermissions();
+	}, []);
 
-  const registry = (type, curp) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+	const registry = (type, curp) => {
+		const myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-      "curp": curp,
-      "tipo": type,
-      "fecha": new Date().toISOString()
-    });
+		const raw = JSON.stringify({
+			curp,
+			tipo: type,
+			fecha: new Date().toISOString().slice(0, 19).replace("T", " "),
+		});
 
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
+		const requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow",
+		};
 
-    fetch("https://XXXXXXXXXXXXXX.firebaseio.com/bitacora.json", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log("TO DO"))
-      .catch(error => console.log('error', error));
-  }
+		fetch(`${process.env.FIREBASE}/attendance.json`, requestOptions)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("HTTP error, status = " + response.status);
+				}
+			})
+			.catch((error) => Alert.alert("Error", error));
+	};
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true)
-    Alert.alert('REGISTRAR', data, [
-      {
-        text: 'SALIDA',
-        onPress: () => registry('SALIDA', data)
-      },
-      {
-        text: 'ENTRADA',
-        onPress: () => registry('ENTRADA', data)
-      },
-    ]);
-  };
+	const handleBarCodeScanned = ({ data }) => {
+		setScanned(true);
+		Alert.alert("REGISTRAR", `CURP: ${data}`, [
+			{
+				text: "SALIDA",
+				onPress: () => registry("SALIDA", data),
+			},
+			{
+				text: "ENTRADA",
+				onPress: () => registry("ENTRADA", data),
+			},
+		]);
+	};
 
-  if (hasPermission === null) {
-    return <Text>Solicitando permido de cámara</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>Sin acceso a la cámara</Text>;
-  }
+	if (hasPermission === null) {
+		return <Text>Solicitando permiso de cámara</Text>;
+	}
 
-  return (
-    <View style={styles.container}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'TOCA PARA ESCANEAR'} onPress={() => setScanned(false)} />}
-    </View>
-  );
+	if (hasPermission === false) {
+		return <Text>Sin acceso a la cámara</Text>;
+	}
+
+	return (
+		<View style={styles.container}>
+			<BarCodeScanner
+				onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+				style={StyleSheet.absoluteFillObject}
+			/>
+			{scanned && <Button title={"TOCA PARA ESCANEAR"} onPress={() => setScanned(false)} />}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		flexDirection: "column",
+		justifyContent: "center",
+	},
 });
