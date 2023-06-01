8ff8187e7e4d8f5cbd97b0007948931c539c99a7
Alejandro RM <alejandror45@gmail.com>
2023-05-31 22:59:43 -0600

    Read from xslx

--- qr-generation/index.js
+++ qr-generation/index.js
@@ -1,61 +1,76 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const XLSX = require("xlsx");
 
-function generarImagenQR(qr_code_text) {
-    const url = "https://api.qr-code-generator.com/v1/create?access-token=XXX";
-    const headers = {
-        "Content-Type": "application/json",
-    };
+function fetchQR(qr_code_text) {
+	const url =
+		"https://api.qr-code-generator.com/v1/create?access-token=vyNr57vAfw6PGTQHOSZ2EoBlVPXRDZePQyzOkUs0622ZZc_q1nYY4jhz6ZbZ9Hgw";
+	const headers = {
+		"Content-Type": "application/json",
+	};
 
-    const data = {
-        "qr_code_text": qr_code_text,
-        "image_format": "SVG",
-        "frame_name": "bottom-frame",
-        "download": 1,
-        "frame_icon_name": "vcard",
-        "frame_text": "ASISTENTE"
-    };
+	const data = {
+		qr_code_text: qr_code_text,
+		image_format: "SVG",
+		frame_name: "bottom-frame",
+		download: 1,
+		frame_icon_name: "vcard",
+		frame_text: "ASISTENTE",
+	};
 
-    const folderPath = path.join(__dirname, 'imagenes');
-    const fileName = `${qr_code_text}.svg`;
-    const filePath = path.join(folderPath, fileName);
+	const folderPath = path.join(__dirname, "images");
+	const fileName = `${qr_code_text}.svg`;
+	const filePath = path.join(folderPath, fileName);
 
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
-    }
+	if (!fs.existsSync(folderPath)) {
+		fs.mkdirSync(folderPath);
+	}
 
-    return axios.post(url, data, {
-        headers,
-        responseType: 'stream'
-    })
-        .then(response => {
-            response.data.pipe(fs.createWriteStream(filePath));
-            return new Promise((resolve, reject) => {
-                response.data.on('error', (err) => {
-                    console.error('Error al guardar el archivo:', err);
-                    reject(err);
-                });
-
-                response.data.on('finish', () => {
-                    resolve();
-                });
-            });
-        })
-        .catch(error => {
-            console.error('Error al hacer la solicitud:', error);
-            throw error;
-        });
+	return axios
+		.post(url, data, {
+			headers,
+			responseType: "stream",
+		})
+		.then((response) => {
+			return new Promise((resolve, reject) => {
+				const writer = fs.createWriteStream(filePath);
+				response.data.pipe(writer);
+				writer.on("finish", resolve);
+				writer.on("error", reject);
+			});
+		})
+		.catch((error) => {
+			console.error("Error al hacer la solicitud:", error);
+			throw error;
+		});
 }
 
-function generarImagenesQR(arrayQR) {
-    const promises = arrayQR.map(qr_code_text => {
-        return generarImagenQR(qr_code_text)
-            .catch(error => console.error(`Error al generar la imagen QR para ${qr_code_text}:`, error));
-    });
+async function main() {
+	const workbook = XLSX.readFile("./source.xlsx");
+	const sheetName = workbook.SheetNames[0];
+	const sheet = workbook.Sheets[sheetName];
+	const startRow = 2;
+	const endRow = sheet["!ref"].split(":").pop().replace(/\D/g, "");
+	const curps = [];
+	for (let row = startRow; row <= endRow; row++) {
+		const cellAddress = `G${row}`;
+		const cell = sheet[cellAddress];
+		const value = (cell && cell.v) || "";
+		curps.push(value.toUpperCase());
+	}
 
-    return Promise.all(promises);
+	try {
+		for (const curp of curps) {
+			try {
+				await fetchQR(curp);
+			} catch (error) {
+				console.error(`Error al generar la imagen QR para ${curp}:`, error);
+			}
+		}
+	} catch (error) {
+		console.error("Error al generar las imágenes QR:", error);
+	}
 }
 
-const arrayQR = ["ROTH020414MQRJMLA1"];
-generarImagenesQR(arrayQR);
+main();
